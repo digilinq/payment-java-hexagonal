b0c0a1cd524f268b4f3afe3640f3f689371196ce
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-04-10 17:43:02 +0200

    Initial commit

---
+++ infrastructure/web/src/main/java/com/eightbits/payment/eft/eftpayment/EftPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.eightbits.payment.eft.eftpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EftPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EftPaymentApplication.class, args);
+	}
+
+}
---
+++ infrastructure/web/src/test/java/com/eightbits/payment/eft/eftpayment/EftPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eightbits.payment.eft.eftpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EftPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
